6e23993e711290ad8d393d3f11578d7825b1e954
Captain <mads130f@edu.easj.dk>
2018-03-08 11:51:09 +0100

    Strategy 2,0

--- src/Strategy/Strategytext.java
+++ src/Strategy/Strategytext.java
@@ -4,6 +4,9 @@ public class Strategytext {
     public static void main(String[] args) {
         //Tillader et program at have flere måder f.eks. at komme fra A til B.
         //F.eks. bus eller bil.
+        //Eller
+        //Betalingservice.
+        //man kan betale med creditkort, mobilospay eller cash
         Context context = new Context(new OperationAdd());
         System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
 

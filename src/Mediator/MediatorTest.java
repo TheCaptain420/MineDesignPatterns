cdfa3c8eed7dda0a82923a8752eb675f0ac0e00b
Captain <mads130f@edu.easj.dk>
2018-03-08 11:26:15 +0100

    Strategy

--- src/Mediator/MediatorTest.java
+++ src/Mediator/MediatorTest.java
@@ -1,6 +1,10 @@
 package Mediator;
 
 public class MediatorTest {
+    //En mediator, er en klasse som andre kommunikere igennem. f.eks. Mail liste
+    // /
+    //Hvis man sender en besked til mange, sender den ikke til hver enkelte, men i stedet til Mediator. Og så sende mediator den till alle sammen.
+
     public static void main(String[] args) {
         User robert = new User("Robert");
         User john = new User("John Hitler");
